06ced931d112927a7a279819dd6afd3ac3b4750e
Ayellet Tsachi <ayelletts@gmail.com>
2022-07-26 15:00:09 +0300

    get

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(cors());
 
 app.use(express.json());
 
+app.get("/", (res) => {
+  res.send("isAlive");
+});
+
 app.use("/files", filesRouter);
 app.listen(process.env.PORT || port, () =>
   console.log(`*** app listening on port ${port}! ***`)
